28104be6696e5b87e8c512e4e16b0f270ff55108
mgh3326 <mgh3326@naver.com>
2017-11-10 10:55:28 +0900

    complete

--- number_generator.py
+++ number_generator.py
@@ -1,9 +1,10 @@
 def number_generator(x, n):
     # 함수를 완성하세요
-    lst = [x]
-    for i in range(n-1):
-            lst.append(x*(i+2))
-    return lst
+    return [i * x + x for i in range(n)]
+    # lst = [x]
+    # for i in range(n-1):
+    #         lst.append(x*(i+2))
+    # return lst
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
 print(number_generator(3,5))
